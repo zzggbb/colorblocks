ec888a0e47e482adcbbce9d50b265f7862cafa13
Zane Bradley <zbradley96@gmail.com>
2015-05-27 16:06:19 -0700

    made gap use default background color, added support for custom character

--- colorblocks.py
+++ colorblocks.py
@@ -4,40 +4,44 @@ import argparse
 import random
 import subprocess
 
-COLORS = range(41, 48)
-choice = random.choice
-clear = lambda: subprocess.call('clear')
+COLORS = range(31, 38)
+clear = lambda: subprocess.call(['tput','reset'])
 echo = lambda s: print(s, end="")
-block = lambda color, size: "\033[0;{}m{}".format(color, " " * size)
 termxy = lambda: map(int, subprocess.check_output(['stty','size']).split())
 showcur = lambda vis: echo('\033[?25{}'.format('h' if vis else 'l'))
-color = lambda row_color, gap, same: \
-    40 if random.random() <= gap else row_color if same else choice(COLORS)
+
+def block(gap, size, row_color, same, char):
+    """
+    generate a block, which is either a space (" ") colored with the default
+    background color, or a char (defaults to ▉) colored with a random color.
+    """
+    colored = gap <= random.random()
+    color = 49 if not colored else row_color if same else random.choice(COLORS)
+    char = char if colored else " "
+    return "\033[0;{}m{}".format(color, char * size)
 
 def distribute(min, max, total, current=0):
     """
     generate a list of numbers between `min` and `max` that adds up to `total`
     """
-    if current == total:
-        return [0]
-    if (total - current) < (max):
+    if total - current < max:
         return [total - current]
-    block = choice(range(min, max+1))
+    block = random.choice(range(min, max+1))
     return [block] + distribute(min, max, total, current+block)
 
-def blocks(min, max, gap, same):
+def blocks(min, max, gap, same, char):
     """
     the width and height of the terminal is calculated, and then a distribution
     of blocks is calculated and printed to perfectly fill the terminal
     """
     if min > max:
         raise ValueError('min-width must be less than or equal to max-width')  
+    rows, cols = termxy()
     try:
-        rows, cols = termxy()
         for row in range(rows):
-            row_color = choice(COLORS)
+            row_color = random.choice(COLORS)
             for size in distribute(min, max, cols):
-                echo(block(color(row_color, gap, same), size)) 
+                echo(block(gap, size, row_color, same, char))
 
         showcur(False)
         input()
@@ -50,8 +54,9 @@ def blocks(min, max, gap, same):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     add = parser.add_argument
-    add('-min',help="set the min width of a block",type=int,default=2)
+    add('-min',help="set the min width of a block",type=int,default=4)
     add('-max',help="set the max width of a block",type=int,default=10)
     add('-gap',help='probablility of a block being a gap',type=float,default=0.3)
     add('-same',help="make all blocks within a row have the same color",action='store_true')
+    add('-char',help="the character that composes a block, default is a block",default="▉")
     blocks(**vars(parser.parse_args()))
